fb57da19f70b3de904e834bae69017c8a088ffad
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-19 13:37:46 +0400

    Some small improvements

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ fn main() {
     let config = clone_config();
     println!("{:?}", config);
 
-    // let alfabet_map = elp_service::get_alfabet();
-
     let mut num_msg = config.num_msg;
 
     while num_msg > 0 {
@@ -19,24 +17,6 @@ fn main() {
         let msg = get_message(config.msg_type.as_str());
 
         let msg_say = prepare_message_for_say(&msg);
-
-        // let mut skip = false;
-        // let mut tmp = "".to_string();
-
-        // for symbol in msg.chars() {
-        //     if symbol == '!' {
-        //         skip = true;
-        //     } else if symbol == ' ' || symbol == ',' {
-        //         tmp = tmp.trim().to_string();
-        //         tmp.push(symbol);
-        //         skip = false;
-        //     } else if alfabet_map.contains_key(symbol.to_string().as_str()) && !skip {
-        //         tmp.push_str(alfabet_map[symbol.to_string().as_str()]);
-        //         tmp.push(' ');
-        //     } else {
-        //         tmp.push(symbol);
-        //     }
-        // }
 
         // say("Pilot".to_string(), false, config.comma).unwrap();
         println!("{}", msg.replace('!', ""));
